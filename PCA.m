7a899c127b0d2675cf7cd53625c297e13fa6ac06
exiledGnar <135248750+exiledGnar@users.noreply.github.com>
2023-11-04 13:37:09 +0800

    Update PCA.m

--- PCA.m
+++ PCA.m
@@ -50,43 +50,10 @@ y = X(1:657, 1);  % 标签向量，训练数据
 features = X(1:657, 2:6);  % 特征值矩阵,训练
 
 % 使用 PCA 进行降维
-numComponents = 1;  % 设置主成分的维度数量
+numComponents = 5;  % 设置主成分的维度数量
 [coeff, score, ~, ~, explained] = pca(features);  % 使用 PCA 进行降维
 selectedComponents = coeff(:, 1:numComponents);  % 选择前 numComponents 个主成分
 
 % 将特征值矩阵投影到所选的主成分上
 projectedFeatures = features * selectedComponents;
-
-% 分类器训练
-classifier = fitcdiscr(projectedFeatures, y);  % 使用降维后的特征值矩阵训练分类器
-
-% 分类器预测
-x1=X(658:757, 2:6)* selectedComponents;
-predictions = predict(classifier, x1);  % 对特征值矩阵进行分类预测
-
-% 计算评估指标
-y1=X(658:757, 1);
-confusionMat = confusionmat(y1, predictions);  % 计算混淆矩阵
-truePositives = confusionMat(2, 2);  % 真正例数目
-falsePositives = confusionMat(1, 2);  % 假正例数目
-trueNegatives = confusionMat(1, 1);  % 真反例数目
-falseNegatives = confusionMat(2, 1);  % 假反例数目
-
-sensitivity = truePositives / (truePositives + falseNegatives);  % 计算敏感度 (SE)
-specificity = trueNegatives / (trueNegatives + falsePositives);  % 计算特异度 (SP)
-accuracy = (truePositives + trueNegatives) / sum(sum(confusionMat));  % 计算准确率 (ACC)
-
-[~, ~, ~, auc] = perfcurve(y1, predictions, 1);  % 计算曲线下面积 (AUC)
-
-% 显示结果
-disp('Confusion Matrix:');
-disp(confusionMat);
-disp('Sensitivity (SE):');
-disp(sensitivity);
-disp('Specificity (SP):');
-disp(specificity);
-disp('Accuracy (ACC):');
-disp(accuracy);
-disp('Area Under the Curve (AUC):');
-disp(auc);
 
